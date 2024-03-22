b9257b67775d92338e1f1c3171b7210d93b6fa78
oliverfont <oliverfont@protonmail.com>
2024-03-22 14:31:38 +0100

    refonte avec yarn + map routage Home et Apropos

---
+++ src/pages/Apropos/index.jsx
@@ -0,0 +1,14 @@
+import '../styles/Home.css';
+import Header from '../Home/Header'
+import Footer from '../Home/Footer'
+
+function App() {
+  return (
+    <div className='body'>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Banner.js
@@ -0,0 +1,11 @@
+import background from './assets/IMG.png'
+import '../styles/Banner.css'
+
+function Banner() {
+    return <div className="banner">
+                <img src={background} alt='background banner' className='bgBanner' />
+                <h1>Chez vous, partout et ailleurs</h1>
+            </div>
+}
+
+export default Banner
