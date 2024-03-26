11c9d8e0bc43b3525566ae73a71a712243008b50
oliverfont <oliverfont@protonmail.com>
2024-03-26 14:52:34 +0100

    MEP de toutes les pages sauf 404; CSS+routes=ok

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/Home/index.jsx';
 import Apropos from './pages/Apropos/index.jsx';
+import Article from './pages/Article/index.jsx';
+import NotFound from './pages/NotFound/index.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
@@ -12,6 +14,8 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/apropos" element={<Apropos />} />
+        <Route path="/article/:id" element={<Article />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   </React.StrictMode>,
--- src/pages/Home/Articles.js
+++ src/pages/Home/Articles.js
@@ -1,17 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Importez Link depuis react-router-dom
 import logements from './data/logements.json';
 import '../styles/Articles.css';
 
 function Articles() {
-    return (
-        <div className="articles">
-            {logements.map((logement, index) => (
-                <div className="article" key={index}>
-                    <h2>{logement.title}</h2>
-                    <img src={logement.cover} alt={`Image de "${logement.title}"`} className="imgArticle" />
-                </div>
-            ))}
-        </div>
-    );
+  return (
+    <div className="articles">
+      {logements.map((logement, index) => (
+        <Link to={`/article/${index}`} key={index} className="article-link">
+          <div className="article">
+            <h2>{logement.title}</h2>
+            <img src={logement.cover} alt={`Image de "${logement.title}"`} className="imgArticle" />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default Articles;
