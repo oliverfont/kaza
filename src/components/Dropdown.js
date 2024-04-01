1d2b373c939b101d83efca607eaf36120c5c47a2
oliverfont <oliverfont@protonmail.com>
2024-04-01 16:35:17 +0200

    dropdowns ok

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/Dropdown.css';
-import Arrow from '../assets/img/arrow.svg'
+import Arrow from '../assets/img/arrow.svg';
 
-function Dropdown({ title, content, selectedTitle, onClick }) {
-    const isOpen = selectedTitle === title;
-  
-    return (
-      <div className={`dropdowns ${isOpen ? 'open' : ''}`}>
-        <div className="dropdown" onClick={() => onClick(title)}>
-          <h3>{title}</h3><img src={Arrow} className='arrow'></img>
-        </div>
-        {isOpen && (
-          <div className="description">
-            <p>{content}</p>
-          </div>
+function Dropdown({ title, content, isOpen, onClick }) {
+  const [open, setOpen] = useState(isOpen);
+
+  const handleClick = () => {
+    setOpen(!open);
+    onClick(title);
+  };
+
+  return (
+    <div className={`dropdowns ${open ? 'open' : ''}`}>
+      <div className="dropdown" onClick={handleClick}>
+        <h3>{title}</h3>
+        <img src={Arrow} className={`arrow ${open ? 'open' : ''}`} alt="Arrow" />
+      </div>
+      <div className={`description ${open ? 'open' : ''}`}>
+        {typeof content === 'string' ? (
+          <p>{content}</p>
+        ) : (
+          <ul>
+            {content.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
         )}
       </div>
-    );
-  }  
+    </div>
+  );
+}
 
 export default Dropdown;
