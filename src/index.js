c58733642c8438d9f9ff764b84b2898194464f67
oliverfont <oliverfont@protonmail.com>
2024-04-11 13:29:48 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')).render(
     <Router>
       <Header />
       <Routes>
-        <Route path="/kaza"  element={<Home />} />
+        <Route path="/"  element={<Home />} />
         <Route path="/apropos" element={<Apropos />} />
         <Route path="/article/:id" element={<Article />} />
         <Route path="*" element={<NotFound />} />
