40218e91a7ea5778ea7e753a48f76a90e8053d2b
oliverfont <oliverfont@protonmail.com>
2024-04-15 02:41:30 +0200

    use hash for gh-pages

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import Home from './pages/Home/index.jsx';
-import Apropos from './pages/Apropos/index.jsx';
-import Article from './pages/Article/index.jsx';
-import NotFound from './pages/NotFound/index.jsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
-import Header from './components/Header.js';
-import Footer from './components/Footer.js';
 
-createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+import Home from './pages/Home';
+import About from './pages/About';  
+import Article from './pages/Article';
+import NotFound from './pages/NotFound';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import reportWebVitals from './reportWebVitals';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
     <Router>
       <Header />
-      <Routes>
-        <Route path="/kasa/home"  element={<Home />} />
-        <Route path="/kasa/apropos" element={<Apropos />} />
-        <Route path="/kasa/article/:id" element={<Article />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+          <Route path="/about" element={<About />} />
+        
+          <Route path="/article/:id" element={<Article />} />
+        
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      
       <Footer />
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 reportWebVitals();
